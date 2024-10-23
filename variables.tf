87c233a5cd13aae7e671fa2edf7ed2e2eb790a6d
Kavalchuk <Aliaksei.Kavalchuk@equiniti.com>
2024-10-23 16:15:48 +0200

    test1

--- variables.tf
+++ variables.tf
@@ -1,15 +1,25 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type = string
+}
+
 variable "region" {
   description = "The region, where the resources are created"
-  type = "string"
+  type = string
   default = "us-east-1"
 }
 
 variable "user_name" {
   description = "IAM username"
-  type = "string"
+  type = string
 }
 
 variable "policy_arn" {
-  description = "ARN policy for user"
-  type = "string"
+  description = "ARN policy for user1"
+  type = string
 }
